93bfb6b64537a4b06997e1a5028ddeb266da92ea
Badiane95 <badiane.falou95@gmail.com>
2025-10-16 12:44:20 +0200

    Mongo atlas config

--- code/env/.env.dev.dist.sh
+++ code/env/.env.dev.dist.sh
@@ -1,2 +1,7 @@
 
 copy .env.dev.dist .env.dev.local
+# Ancienne configuration (locale)
+# MONGODB_URI=mongodb://localhost:27017/sae501
+
+# Nouvelle configuration (Atlas Cloud)
+mongodb+srv://SAE501_db_user:jT1dsu1rG1dnZIoS@sae501.mxjorrf.mongodb.net/?retryWrites=true&w=majority&appName=SAE501
